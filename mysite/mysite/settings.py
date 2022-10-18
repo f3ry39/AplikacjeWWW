7e2bc5bc73494b90c6c15f463327089b634358e8
Bartosz Gryciuk <f3ry39@gmail.com>
2022-10-18 18:10:55 +0200

    config database and create polls

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aplikacjewww',
+        'USER': 'postgres',
+        'PASSWORD': 'bartek',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
