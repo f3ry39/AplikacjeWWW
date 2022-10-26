5bbe361f832f8c7e0fa56dfe98a825aeb17dfbac
Bartosz Gryciuk <f3ry39@gmail.com>
2022-10-26 10:34:56 +0200

    zadanie 10

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -6,6 +6,7 @@ from .models import Druzyna
 
 class OsobaAdmin(admin.ModelAdmin):
     list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', 'data_dodania', 'kraj']
+    list_filter = ['kraj', 'data_dodania']
 class DruzynaAdmin(admin.ModelAdmin):
     list_display = ['nazwa', 'kraj']
     list_filter = ['nazwa']
