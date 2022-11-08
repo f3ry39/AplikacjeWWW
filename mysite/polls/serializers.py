3ca0e77ddc7fce254fd553f4474a1698cd5fa631
Bartosz Gryciuk <f3ry39@gmail.com>
2022-11-08 19:10:57 +0100

    lab4

--- mysite/polls/serializers.py
+++ mysite/polls/serializers.py
@@ -5,8 +5,9 @@ class OsobaSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     imie = serializers.CharField(required=True)
     nazwisko = serializers.CharField(required=True)
-    miesiac_urodzenia = serializers.ChoiceField(choices=MIESIAC_URODZENIA.choices, default=MIESIAC_URODZENIA.choices[0][0])
-    kraj = serializers.PrimaryKeyRelatedField(queryset=Druzyna.objects.all())
+    miesiac_urodzenia = serializers.ChoiceField(choices=MIESIAC_URODZENIA, default=MIESIAC_URODZENIA[0][0])
+    kraj = serializers.PrimaryKeyRelatedField(queryset=Druzyna.objects.all(), allow_null=True)
+
 
     def create(self, validated_data):
         return Osoba.objects.create(**validated_data)
@@ -16,6 +17,19 @@ class OsobaSerializer(serializers.Serializer):
         instance.nazwisko = validated_data.get('nazwisko', instance.nazwisko)
         instance.miesiac_dodania = validated_data.get('miesiac_dodania', instance.miesiac_dodania)
         instance.kraj = validated_data.get('kraj', instance.kraj)
+        instance.save()
+        return instance
+class DruzynaSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    kraj = serializers.CharField(required=True)
+    nazwa = serializers.CharField(required=True)
+
+    def create(self, validated_data):
+        return Druzyna.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.kraj = validated_data.get('kraj', instance.kraj)
+        instance.nazwa = validated_data.get('nazwa', instance.nazwa)
         instance.save()
         return instance
 class QuestionModelSerializer(serializers.ModelSerializer):
